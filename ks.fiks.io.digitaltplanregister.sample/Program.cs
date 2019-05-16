6949c3af074622a2d30c7d9794454b9f8f8c8dfb
Tor Kjetil Nilsen <tor@arkitektum.no>
2019-05-16 10:53:58 +0200

    Add project files.

---
+++ ks.fiks.io.digitaltplanregister.sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ks.fiks.io.digitaltplanregister.sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
