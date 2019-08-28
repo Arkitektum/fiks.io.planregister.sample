b6713977a3061e262761df974efcfcaa88cb371c
Tor Kjetil Nilsen <tor@arkitektum.no>
2019-08-28 12:49:09 +0200

    init planregister sample

--- ks.fiks.io.digitaltplanregister.sample/Program.cs
+++ ks.fiks.io.digitaltplanregister.sample/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Ks.Fiks.Maskinporten.Client;
+using KS.Fiks.IO.Client;
+using KS.Fiks.IO.Client.Configuration;
+using KS.Fiks.IO.Client.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace ks.fiks.io.digitaltplanregister.sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<PlanregisterService>();
+            })
+            .RunConsoleAsync();
         }
+
     }
 }
